ae235e8b47c06b21020ed8d81353f15d0bfd89cb
Hum9183 <hum9183@gmail.com>
2024-03-24 11:54:49 +0900

    1

--- cv_freezer/main.py
+++ cv_freezer/main.py
@@ -3,74 +3,103 @@ from maya import cmds, mel
 
 maya_version = int(cmds.about(v=True))
 after_maya2022 = maya_version >= 2022
-before_maya2020 = not after_maya2022
 
 TWEAK = 'tweak'
 
 
 def create_poly_tweak_uv_history(mesh):
-    cmds.select('{}.map[0]'.format(mesh), r=True)
-    cmds.polyEditUV(u=1)
-    cmds.polyEditUV(u=-1)
+    """polyTweakUVヒストリを生成する"""
+    cmds.select('{}.map[0]'.format(mesh), r=True)   # 0番のUVを選択する
+    cmds.polyEditUV(u=1)                            # U軸を1移動する
+    cmds.polyEditUV(u=-1)                           # U軸を-1移動する(もとに戻す)
+    cmds.select(cl=True)
+
+
+def delete_history(mesh):
+    """「ヒストリ」を削除する"""
+    cmds.select(mesh)
+    mel.eval('DeleteHistory')
     cmds.select(cl=True)
 
 
 def delete_non_deformer_history(mesh):
+    """「デフォーマ以外のヒストリ」を削除する"""
     cmds.select(mesh)
     mel.eval('BakeNonDefHistory')
     cmds.select(cl=True)
 
 
 def get_history(mesh, sort_type):
+    """ヒストリを取得する"""
     histories = cmds.listHistory(mesh)
     histories = cmds.ls(histories, type=sort_type)
     return histories[0] if histories else None
 
 
 def has_tweak(mesh):
+    """tweakを持っているかどうか"""
     tweak = get_history(mesh, TWEAK)
     return tweak is not None
 
 
 def add_tweak(mesh):
+    """tweakを追加する"""
     cmds.select(mesh, r=True)
     mel.eval('AddTweak')
     cmds.select(cl=True)
 
 
 def delete_tweak(mesh):
+    """tweakを削除する"""
     tweak = get_history(mesh, TWEAK)
     cmds.delete(tweak)
 
 
-def freeze_cv_with_tweak(mesh):
+def has_deformers(mesh):
+    deformers = cmds.findDeformers(mesh)
+    return deformers is not None
+
+
+def freeze_cv_with_deformers_and_tweak(mesh):
+    """CV値をフリーズする(deformerあり、tweakあり)"""
     create_poly_tweak_uv_history(mesh)
     delete_non_deformer_history(mesh)
 
 
-def freeze_cv_without_tweak(mesh):
+def freeze_cv_with_deformers_without_tweak(mesh):
+    """CV値をフリーズする(deformerあり、tweakなし)"""
     add_tweak(mesh)
     create_poly_tweak_uv_history(mesh)
     delete_non_deformer_history(mesh)
     delete_tweak(mesh)
 
 
-# TODO: 未バインド(NonDeformer)、BS、などのパターンも網羅する
-def freeze_cv():
+def freeze_cv_without_deformers(mesh):
+    """CV値をフリーズする(deformerなし)"""
+    mel.eval('CreateLattice')
+    delete_history(mesh)
+
+
+def main():
+    """ツールの起点"""
     mesh = cmds.ls(sl=True)[0]
-    _has_tweak = has_tweak(mesh)
-    if _has_tweak:
-        freeze_cv_with_tweak(mesh)
+    _has_deformers = has_deformers(mesh)
+    if _has_deformers:
+        _has_tweak = has_tweak(mesh)
+        if _has_tweak:
+            freeze_cv_with_deformers_and_tweak(mesh)
+        else:
+            freeze_cv_with_deformers_without_tweak(mesh)
     else:
-        freeze_cv_without_tweak(mesh)
-    print(u'CVをフリーズしました。')
+        freeze_cv_without_deformers(mesh)
+    print(u'CV値をフリーズしました。')
 
 
-# Startup command
+# # Startup command
 # from cv_freezer.cv_freezer import main as cv_freezer_main
 # import importlib
 # try:
 #     importlib.reload(cv_freezer_main)
 # except AttributeError:
 #     reload(cv_freezer_main)
-# cv_freezer_main.freeze_cv()
+# cv_freezer_main.main()
--- tests/test.py
+++ tests/test.py
@@ -5,62 +5,116 @@ import unittest
 from maya import cmds, mel
 
 from ..cv_freezer import main
-from . import test_utils as utils
+
+TEST_CV_VALUE = -2      # テスト時に設定するCV値。-2でなくてもよい
+FROZEN_CV_VALUE = 0     # フリーズ後のCV値。assertEqual()で比較する
+DEFORMATION_CREATE_TWEAK = 'deformationCreateTweak'
+
+
+def get_deformation_create_tweak_op_var():
+    """optionVar「deformationCreateTweak」の値を取得する"""
+    return cmds.optionVar(q=DEFORMATION_CREATE_TWEAK)
+
+
+def set_deformation_create_tweak_op_var(value):
+    """optionVar「deformationCreateTweak」の値を設定する"""
+    cmds.optionVar(intValue=(DEFORMATION_CREATE_TWEAK, value))
+
+
+def create_skin_bound_cube():
+    """スキンバインドされたCubeを生成する"""
+    cube = cmds.polyCube()[0]
+    cmds.select(cl=True)
+    joint = cmds.joint()
+    cmds.select(cl=True)
+    sel = [cube, joint]
+    cmds.select(sel, r=True)
+    mel.eval('SmoothBindSkin')
+    return cube
+
+
+def set_vtx0_cv(mesh, tweak, value):
+    """0番の頂点のx座標にCV値を設定する"""
+    if tweak is None:
+        cmds.setAttr('{}.pnts[0].pntx'.format(mesh), value)
+    else:
+        cmds.setAttr('{}.vlist[0].vertex[0].xVertex'.format(tweak), value)
+
+
+def get_vtx0_cv(mesh, tweak):
+    """0番の頂点のx座標にCV値を取得する"""
+    if tweak is None:
+        vtx0_cv = cmds.getAttr('{}.pnts[0].pntx'.format(mesh))
+    else:
+        vtx0_cv = cmds.getAttr('{}.vlist[0].vertex[0].xVertex'.format(tweak))
+    return vtx0_cv
+
+
+def new_scene():
+    """新規シーンを開く"""
+    cmds.file(force=True, new=True)
 
 
 class CVFreezerTestCase(unittest.TestCase):
     def setUp(self):
-        utils.new_scene()
+        """テストメソッドの実行前に呼ばれる"""
+        new_scene()
 
-    # TODO: 未バインド、BSなどのパターンも網羅する
-    def test_freeze_cv_with_tweak(self):
+    def test_freeze_cv_with_deformers_and_tweak(self):
+        """freeze_cv_with_deformers_and_tweakのテストメソッド"""
         deformation_create_tweak_orig_value = None
         if main.after_maya2022:
             # 2022以降はオプションでtweakありかなしかを変更できる
             # このテストケースでは強制的にありにする
-            deformation_create_tweak_orig_value = utils.get_deformation_create_tweak_op_var()
-            utils.set_deformation_create_tweak_op_var(True)
-        cube = utils.create_skin_bound_cube()
+            deformation_create_tweak_orig_value = get_deformation_create_tweak_op_var()
+            set_deformation_create_tweak_op_var(True)
+        cube = create_skin_bound_cube()
         tweak = main.get_history(cube, main.TWEAK)
-        utils.set_vtx0_cv(cube, tweak)
-        cmds.select(cube)
-        main.freeze_cv()
-        vtx0_cv = utils.get_vtx0_cv(cube, tweak)
-        self.assertEqual(vtx0_cv, 0)
+        set_vtx0_cv(cube, tweak, TEST_CV_VALUE)
+        main.freeze_cv_with_deformers_and_tweak(cube)
+        vtx0_cv = get_vtx0_cv(cube, tweak)
+        self.assertEqual(vtx0_cv, FROZEN_CV_VALUE)
         if main.after_maya2022:
-            utils.set_deformation_create_tweak_op_var(deformation_create_tweak_orig_value)
+            set_deformation_create_tweak_op_var(deformation_create_tweak_orig_value)
 
-    def test_freeze_cv_without_tweak(self):
+    def test_freeze_cv_with_deformers_without_tweak(self):
+        """freeze_cv_without_tweakのテストメソッド"""
         if main.after_maya2022:
             # 2022以降はオプションでtweakありかなしかを変更できる
             # このテストケースでは強制的になしにする
-            deformation_create_tweak_orig_value = utils.get_deformation_create_tweak_op_var()
-            utils.set_deformation_create_tweak_op_var(False)
+            deformation_create_tweak_orig_value = get_deformation_create_tweak_op_var()
+            set_deformation_create_tweak_op_var(False)
         else:
             # 2020以前はtweakは必ずあるため確定でOk
             return
-        cube = utils.create_skin_bound_cube()
+        cube = create_skin_bound_cube()
         tweak = main.get_history(cube, main.TWEAK)
-        utils.set_vtx0_cv(cube, tweak)
-        cmds.select(cube)
-        main.freeze_cv()
-        vtx0_cv = utils.get_vtx0_cv(cube, tweak)
-        self.assertEqual(vtx0_cv, 0)
+        set_vtx0_cv(cube, tweak, TEST_CV_VALUE)
+        main.freeze_cv_with_deformers_without_tweak(cube)
+        vtx0_cv = get_vtx0_cv(cube, tweak)
+        self.assertEqual(vtx0_cv, FROZEN_CV_VALUE)
         if main.after_maya2022:
-            utils.set_deformation_create_tweak_op_var(deformation_create_tweak_orig_value)
+            set_deformation_create_tweak_op_var(deformation_create_tweak_orig_value)
+
+    def test_freeze_cv_without_deformers(self):
+        """freeze_cv_without_deformersのテストメソッド"""
+        cube = cmds.polyCube()[0]
+        tweak = None
+        set_vtx0_cv(cube, tweak, TEST_CV_VALUE)
+        main.freeze_cv_without_deformers(cube)
+        vtx0_cv = get_vtx0_cv(cube, tweak)
+        self.assertEqual(vtx0_cv, FROZEN_CV_VALUE)
 
     def tearDown(self):
-        # self.new_scene()
-        pass
-
-
-def get_test_suite():
-    return unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
+        """テストメソッドの実行後に呼ばれる"""
+        new_scene()
 
 
 def run():
+    """テストの起点"""
+    test_suite = unittest.TestLoader().loadTestsFromModule(sys.modules[__name__])
     runner = unittest.TextTestRunner(verbosity=2)
-    runner.run(get_test_suite())
+    runner.run(test_suite)
 
 
 # Startup command
